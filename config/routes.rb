dd096a0adcdd41612d21a1a083c5119e8c7e9d39
Duy Khanh <khanhcd2110@gmail.com>
2013-08-09 15:36:13 +0700

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,7 @@ Dailyreport3::Application.routes.draw do
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  
-
-
-
-
-
-  # The priority is based upon order of creation: first created -> highest priority.
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
