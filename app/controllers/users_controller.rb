5aad04176cc8141c44450cb7e8dc2f7d846d495a
Duy Khanh <khanhcd2110@gmail.com>
2013-08-12 10:40:15 +0700

    Add signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,42 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@users = User.paginate(page: params[:page])
+  end
+
+  def edit
+  	@user = User.find_by(id: params[:id])
+  end
+
+  def update   #for admin 
+  	@user =User.find(params[:id])
+  	if @user.update_attributes(user_params)
+  		flash[:success]= "Profile updated"
+  		sign_in @user
+  		redirect_to @user
+  	else
+  		render 'edit'
+  	end
+  end
+
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+
+  end
+
+
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :group_id , :manager_group, :active)
+  	end
+
+  	def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
