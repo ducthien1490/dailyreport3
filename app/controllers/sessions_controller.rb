5337da82300c5a1e5a612dedcc43c88cdca7848c
Duc Thien <duc.thien@example.com>
2013-08-12 16:56:38 +0700

    Final version in Mon 12 Aug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,14 @@ def create
 	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       	# Sign the user in and redirect to the user's page.
+      if user.active?
       	sign_in user
       	redirect_to root_url
+      else
+        flash[:error] = 'You are not actived' 
+        render 'new'
+      end
+
     else
     	flash[:error] = 'Invalid email/password combination' 
     	render 'new'
