8ab31f652e14bfdb768b735d0b6a6d05c77298e4
Duy Khanh <khanhcd2110@gmail.com>
2013-08-19 16:54:26 +0700

    last final answer form

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ActiveRecord::Base
 	  belongs_to :user
 	  belongs_to :catalog
-	  validates :content, length: { maximum: 400} # cau tra loi
+	  validates :content, length: { maximum: 800} # cau tra loi
       validates :catalog_id,presence: true
       validates :user_id,presence: true
       default_scope -> { order('created_at DESC') }
